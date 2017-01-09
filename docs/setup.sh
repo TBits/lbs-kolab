255754b1728e966a1282493c950f8cf45cfd61f3
Timotheus Pokorra <tp@tbits.net>
2017-01-09 23:08:05 +0100

    generate the kolabdocs on lbs

---
+++ docs/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+dnf install -y python-setuptools make graphviz-python graphviz-gd python-pillow python-sphinx python-sphinx-latex git which
+git clone --depth 1 http://git.kolab.org/diffusion/D/docs.git
+cd docs
+make html
+make latexpdf
+
+#upload to Hostsharing
+if [ -f ~/.ssh/id_rsa_cronjob ]
+then
+  rsync -avz --delete -e "ssh -o 'StrictHostKeyChecking no' -i ~/.ssh/id_rsa_cronjob" build/html tim00-timotheus@tim00.hostsharing.net:kolabdocs
+  rsync -avz --delete -e "ssh -o 'StrictHostKeyChecking no' -i ~/.ssh/id_rsa_cronjob" build/latex/KolabGroupware.pdf tim00-timotheus@tim00.hostsharing.net:kolabdocs
+fi
